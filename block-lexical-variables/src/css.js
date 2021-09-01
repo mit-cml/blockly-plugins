79bde45b15275f3ad8f3f39a5f7bc84d15fb2c37
mark-friedman <mark-friedman@users.noreply.github.com>
2021-08-31 17:20:00 -0700

    Add and inject extra CSS used by us.

---
+++ block-lexical-variables/src/css.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import * as Blockly from 'blockly/core';
+
+/**
+ * Array making up the extra CSS content for added Blockly fields.
+ */
+const EXTRA_CSS = [
+  '.blocklyFieldParameter>rect {',
+  '  fill: rgb(222, 143, 108);',
+  '  fill-opacity: 1.0;',
+  '  stroke-width: 2;',
+  '  stroke: rgb(231, 175, 150);',
+  '}',
+  '.blocklyFieldParameter>text {',
+  '  stroke-width: 1;',
+  '  fill: #000;',
+  '}',
+  '.blocklyFieldParameter:hover>rect {',
+  '  stroke-width: 2;',
+  '  stroke: rgb(231,175,150);',
+  '  fill: rgb(231,175,150);',
+  '  fill-opacity: 1.0;',
+  '}',
+  '/*',
+  ' * [lyn, 10/08/13] Control flydown with the getter/setter blocks.',
+  ' */',
+  '.blocklyFieldParameterFlydown {',
+  '  fill: rgb(231,175,150);',
+  '  fill-opacity: 0.8;',
+  '}',
+  '/*',
+  ' * [lyn, 10/08/13] Control parameter fields with flydown procedure caller block.',
+  ' */',
+  '.blocklyFieldProcedure>rect {',
+  '  fill: rgb(215,203,218);',
+  '  fill-opacity: 1.0;',
+  '  stroke-width: 0;',
+  '  stroke: #000;',
+  '}',
+  '.blocklyFieldProcedure>text {',
+  '  fill: #000;',
+  '}',
+  '.blocklyFieldProcedure:hover>rect {',
+  '  stroke-width: 2;',
+  '  stroke: #fff;',
+  '  fill: rgb(215,203,218);',
+  '  fill-opacity: 1.0;',
+  '}',
+  '/*',
+  ' * [lyn, 10/08/13] Control flydown with the procedure caller block.',
+  ' */',
+  '.blocklyFieldProcedureFlydown {',
+  '  fill: rgb(215,203,218);',
+  '  fill-opacity: 0.8;',
+  '}',
+];
+
+/**
+ * Register our extra CSS with Blockly.
+ */
+export function registerCss() {
+  Blockly.Css.register(EXTRA_CSS);
+}
--- block-lexical-variables/test/index.js
+++ block-lexical-variables/test/index.js
@@ -11,11 +11,22 @@
 import * as Blockly from 'blockly';
 import {createPlayground} from '@blockly/dev-tools';
 import '../src/index';
-
+import {registerCss} from '../src/css';
 
 // TODO: Edit list of blocks.
 const allBlocks = ['global_declaration', 'controls_forRange'];
 
+function init(workspace) {
+  // TODO: Might need the next line
+  // Blockly.DropDownDiv.createDom();
+  const flydown = new Blockly.Flydown(new Blockly.Options({scrollbars: false}));
+  // ***** [lyn, 10/05/2013] NEED TO WORRY ABOUT MULTIPLE BLOCKLIES! *****
+  workspace.flydown_ = flydown;
+  Blockly.utils.dom.insertAfter(flydown.createDom('g'),
+      workspace.svgBubbleCanvas_);
+  flydown.init(workspace);
+  flydown.autoClose = true; // Flydown closes after selecting a block
+}
 /**
  * Create a workspace.
  * @param {HTMLElement} blocklyDiv The blockly container div.
@@ -23,7 +34,9 @@ const allBlocks = ['global_declaration', 'controls_forRange'];
  * @return {!Blockly.WorkspaceSvg} The created workspace.
  */
 function createWorkspace(blocklyDiv, options) {
+  registerCss();
   const workspace = Blockly.inject(blocklyDiv, options);
+  init(workspace);
   return workspace;
 }
 
