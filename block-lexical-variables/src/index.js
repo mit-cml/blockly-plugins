2bea577f05f8b16219188dcf226cacb46a00d0f4
Mark Friedman <mark-friedman@users.noreply.github.com>
2023-09-28 12:22:17 -0700

    Export plugin as a class
      This is to provide a namespace for clients using script tabs so that all the exports aren't just placed on the global/window object.

--- block-lexical-variables/src/index.js
+++ block-lexical-variables/src/index.js
@@ -15,13 +15,13 @@ registerCss();
 import './utilities.js';
 import './workspace.js';
 import './procedure_utils.js';
-import './fields/flydown.js';
-import './fields/field_flydown.js';
-import './fields/field_global_flydown.js';
+import {Flydown} from './fields/flydown.js';
+import {FieldFlydown} from "./fields/field_flydown.js";
+import {FieldGlobalFlydown} from "./fields/field_global_flydown.js";
 import './fields/field_nocheck_dropdown.js';
-import './fields/field_lexical_variable.js';
-import './fields/field_parameter_flydown.js';
-import './fields/field_procedurename.js';
+import {FieldLexicalVariable} from './fields/field_lexical_variable.js';
+import {FieldParameterFlydown} from './fields/field_parameter_flydown.js';
+import {FieldProcedureName} from './fields/field_procedurename.js';
 import './blocks/lexical-variables.js';
 import './blocks/controls.js';
 import './procedure_database.js';
@@ -30,37 +30,36 @@ import './generators/controls.js';
 import './generators/procedures.js';
 import './generators/lexical-variables.js';
 import * as Blockly from 'blockly/core';
-import {Flydown} from './fields/flydown.js';
 
-// Export Flydown and fields for use by plugin users.
-// Note that we might eb exporting too much here, but let's see how it goes.
-export * from './fields/flydown.js';
-export * from './fields/field_flydown.js';
-export * from './fields/field_global_flydown.js';
-export * from './fields/field_parameter_flydown.js';
-export * from './fields/field_lexical_variable.js';
-export * from './fields/field_nocheck_dropdown.js';
-export * from './fields/field_procedurename.js';
+export class LexicalVariablesPlugin {
 
-/**
- * @param workspace
- */
-export function init(workspace) {
-  // TODO: Might need the next line
-  // Blockly.DropDownDiv.createDom();
-  const flydown = new Flydown(
-      new Blockly.Options({
-        scrollbars: false,
-        rtl: workspace.RTL,
-        renderer: workspace.options.renderer,
-        rendererOverrides: workspace.options.rendererOverrides,
-        parentWorkspace: workspace,
-      })
-  );
-  // ***** [lyn, 10/05/2013] NEED TO WORRY ABOUT MULTIPLE BLOCKLIES! *****
-  workspace.flydown_ = flydown;
-  Blockly.utils.dom.insertAfter(flydown.createDom('g'),
-      workspace.svgBubbleCanvas_);
-  flydown.init(workspace);
-  flydown.autoClose = true; // Flydown closes after selecting a block
+  /**
+   * @param workspace
+   */
+  static init(workspace) {
+    // TODO: Might need the next line
+    // Blockly.DropDownDiv.createDom();
+    const flydown = new Flydown(
+        new Blockly.Options({
+          scrollbars: false,
+          rtl: workspace.RTL,
+          renderer: workspace.options.renderer,
+          rendererOverrides: workspace.options.rendererOverrides,
+          parentWorkspace: workspace,
+        })
+    );
+    // ***** [lyn, 10/05/2013] NEED TO WORRY ABOUT MULTIPLE BLOCKLIES! *****
+    workspace.flydown_ = flydown;
+    Blockly.utils.dom.insertAfter(flydown.createDom('g'),
+        workspace.svgBubbleCanvas_);
+    flydown.init(workspace);
+    flydown.autoClose = true; // Flydown closes after selecting a block
+  }
+
+  static Flydown = Flydown;
+  static FieldFlydown = FieldFlydown;
+  static FieldGlobalFlydown = FieldGlobalFlydown;
+  static FieldParameterFlydown = FieldParameterFlydown;
+  static FieldLexicalVariable = FieldLexicalVariable;
+  static FieldProcedureName = FieldProcedureName;
 }
--- block-lexical-variables/test/index.js
+++ block-lexical-variables/test/index.js
@@ -10,7 +10,7 @@
 
 import * as Blockly from 'blockly/core';
 import {createPlayground} from '@blockly/dev-tools';
-import {init} from '../src/index.js';
+import {LexicalVariablesPlugin} from '../src/index.js';
 
 // TODO: Edit list of blocks.
 const allBlocks = [
@@ -36,7 +36,7 @@ const allBlocks = [
  */
 function createWorkspace(blocklyDiv, options) {
   const workspace = Blockly.inject(blocklyDiv, options);
-  init(workspace);
+  LexicalVariablesPlugin.init(workspace);
   return workspace;
 }
 
