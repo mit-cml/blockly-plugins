143bdef9368dd05dd2b73a24f92a49ee818acb99
Evan W. Patton <ewpatton@mit.edu>
2023-09-29 17:14:30 -0400

    Move blocks to separate import
    Change-Id: Ib9a758cd355f7a7cebdcdfc6df33e6777c60dc58

---
+++ block-lexical-variables/src/blocks.js
@@ -0,0 +1,8 @@
+// -*- mode: java; c-basic-offset: 2; -*-
+// Copyright 2023 MIT, All rights reserved
+// Released under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+import './blocks/lexical-variables.js';
+import './blocks/controls.js';
+import './blocks/procedures.js';
