24bbc3ca783d3267e516d37673b47444a15fb2e1
TannerGabriel <gabrieltanner.code@gmail.com>
2025-08-11 09:37:04 +0200

    Add test for toggling procedure_defreturn stack input

--- block-lexical-variables/test/procedures.mocha.js
+++ block-lexical-variables/test/procedures.mocha.js
@@ -47,6 +47,20 @@ function containsBlocks(block, identifier, isId = true) {
   return false;
 }
 
+/**
+ * Utility function to check if a procedure block has a stack field.
+ * @param procedureBlock - The procedure block to check.
+ * @param stackEnabled - Whether the stack field should be present (true) or not (false).
+ */
+function checkStackFieldExists(procedureBlock, stackEnabled = false) {
+  const stackField = procedureBlock.getInput('STACK');
+  if (stackEnabled) {
+    chai.assert.isDefined(stackField, 'Procedure block should have a stack field');
+  } else {
+    chai.assert.isNull(stackField, 'Procedure block should not have a stack field');
+  }
+}
+
 suite ('Procedures', function() {
   setup(function () {
     this.workspace = new Blockly.Workspace();
@@ -110,5 +124,37 @@ suite ('Procedures', function() {
           chai.assert.isTrue(containsBlocks(block, id), `Block with id ${id} should be contained`);
         });
     })
+
+    test('Mutation button click should enable stack', function() {
+      const xml = Blockly.utils.xml.textToDom(
+        '<xml>' +
+        '  <block type="procedures_defreturn" id="p" x="310" y="213">' +
+        '    <mutation stack_enabled="false">' +
+        '      <arg name="x"></arg>' +
+        '      <arg name="y"></arg>' +
+        '    </mutation>' +
+        '    <field name="NAME">do_something</field>' +
+        '  </block>' +
+        '</xml>'
+      );
+
+      Blockly.Xml.domToWorkspace(xml, this.workspace);
+      const block = this.workspace.getBlockById('p');
+      chai.assert.isDefined(block, 'Procedure block should be defined');
+
+      const mutation = block.mutationToDom();
+      chai.assert.equal(mutation.getAttribute('stack_enabled'), 'false', 'stack_enabled should initially be false');
+      checkStackFieldExists(block, false);
+
+      block.domToMutation(Blockly.utils.xml.textToDom(
+        '<mutation stack_enabled="true">' +
+        '  <arg name="x"></arg>' +
+        '  <arg name="y"></arg>' +
+        '</mutation>'
+      ));
+      const updatedMutation = block.mutationToDom();
+      chai.assert.equal(updatedMutation.getAttribute('stack_enabled'), 'true', 'stack_enabled should be true after toggle');
+      checkStackFieldExists(block, true);
+    });
   })
 })
